0a6251590aff6a3ffe0b2be592e53a2bef137987
Marina <boulaimarina@gmail.com>
2025-02-04 16:14:01 +0100

    relative path test

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,11 @@
 const navbarContent = `
      <span class="logo">Marina.</span>
         <ul>
-            <li><a href="../Trotiflex.github.io/vues/curriculum-vitae.html">CV</a></li>
+            <li><a href="/vues/curriculum-vitae.html">CV</a></li>
             <li><a href="index.html" class="active">compétences</a></li>
-            <li><a href="../Trotiflex.github.io/vues/Work.html">Travaux</a></li>
-            <li><a href="../Trotiflex.github.io/vues/book.html">Book</a></li>
-            <li><a href="../Trotiflex.github.io/vues/vt.html">Veille Technologique</a></li>
+            <li><a href="/vues/Work.html">Travaux</a></li>
+            <li><a href="/vues/book.html">Book</a></li>
+            <li><a href="/vues/vt.html">Veille Technologique</a></li>
 
         </ul>
 `;
