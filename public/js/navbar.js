532ee29b431b85750fd76e4cb43bb43dd825d556
Marina <boulaimarina@gmail.com>
2025-02-04 16:08:05 +0100

    link and base files created

---
+++ public/js/navbar.js
@@ -0,0 +1,17 @@
+const navbarContent = `
+     <span class="logo">Marina.</span>
+        <ul>
+            <li><a href="../Trotiflex.github.io/vues/curriculum-vitae.html">CV</a></li>
+            <li><a href="index.html" class="active">compétences</a></li>
+            <li><a href="../Trotiflex.github.io/vues/Work.html">Travaux</a></li>
+            <li><a href="../Trotiflex.github.io/vues/book.html">Book</a></li>
+            <li><a href="../Trotiflex.github.io/vues/vt.html">Veille Technologique</a></li>
+
+        </ul>
+`;
+
+(() => {
+    let balisenavbar = document.getElementById("navbar");
+    balisenavbar.innerHTML = navbarContent;
+})();
+
