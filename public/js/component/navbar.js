c51951b4c5d73ca00350e138e376f88c45af239c
Marina <boulaimarina@gmail.com>
2025-02-27 17:13:40 +0100

    kinematic inverse project 3 start

--- public/js/component/navbar.js
+++ public/js/component/navbar.js
@@ -1,9 +1,9 @@
 const navbarContent = `
              <ul>
             <li><a href="../index.html">About me</a></li>
-            <li><a href="../vues/contact.html">Contact</a></li>
             <li><a href="../vues/watch.html">Technology Watch</a></li>
-            
+            <li><a href="../vues/Documents.html">Documents</a></li>
+            <li><a href="../vues/contact.html">Contact</a></li>
 
         </ul>
         <div class="border-gradient"></div>
