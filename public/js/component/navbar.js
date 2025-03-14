8991f2aaefcc38593fadb99fe60417c63e480732
Marina <boulaimarina@gmail.com>
2025-03-14 14:10:57 +0100

    dfhfhjfbgjd navbar

--- public/js/component/navbar.js
+++ public/js/component/navbar.js
@@ -1,16 +1,43 @@
 const navbarContent = `
-             <ul>
+    <div class="menu-burger" id="menu-burger">☰</div>
+    <div class="overlay" id="overlay"></div>
+
+    <!-- Version Desktop -->
+    <ul class="desktop-menu">
+        <li><a href="../index.html">About me</a></li>
+        <li><a href="../vues/watch.html">Technology Watch</a></li>
+        <li><a href="../vues/Documents.html">Documents</a></li>
+        <li><a href="../vues/contact.html">Contact</a></li>
+    </ul>
+
+    <div class="navbar-mobile" id="navbar-mobile">
+        <ul>
             <li><a href="../index.html">About me</a></li>
             <li><a href="../vues/watch.html">Technology Watch</a></li>
             <li><a href="../vues/Documents.html">Documents</a></li>
             <li><a href="../vues/contact.html">Contact</a></li>
-
         </ul>
         <div class="border-gradient"></div>
+    </div>
 `;
 
 (() => {
-    let balisenavbar = document.getElementById("navbar");
-    balisenavbar.innerHTML = navbarContent;
-})();
+    let navbarContainer = document.getElementById("navbar");
+    navbarContainer.innerHTML = navbarContent;
 
+    const menuBurger = document.getElementById("menu-burger");
+    const navbarMobile = document.getElementById("navbar-mobile");
+    const overlay = document.getElementById("overlay");
+
+    // Ouvrir le menu mobile
+    menuBurger.addEventListener("click", () => {
+        navbarMobile.style.right = "0";
+        overlay.style.display = "block";
+    });
+
+    // Fermer le menu en cliquant sur l'overlay
+    overlay.addEventListener("click", () => {
+        navbarMobile.style.right = "-250px";
+        overlay.style.display = "none";
+    });
+})();
