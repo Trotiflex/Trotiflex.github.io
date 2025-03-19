3d3df47cdc1430383ec4d940283d5220e90fe05b
Marina <boulaimarina@gmail.com>
2025-03-19 16:57:42 +0100

    portfolio almost finished

--- public/js/component/navbar.js
+++ public/js/component/navbar.js
@@ -7,7 +7,11 @@ const navbarContent = `
         <li><a href="../index.html">About me</a></li>
         <li><a href="../vues/watch.html">Technology Watch</a></li>
         <li><a href="../vues/Documents.html">Documents</a></li>
-        <li><a href="../vues/contact.html">Contact</a></li>
+        <li>
+            <a href="https://github.com/Trotiflex" target="_blank">
+                <img src="../public/image/github.png" alt="GitHub" class="github-icon" />
+            </a>
+        </li>
     </ul>
 
     <div class="navbar-mobile" id="navbar-mobile">
@@ -15,9 +19,12 @@ const navbarContent = `
             <li><a href="../index.html">About me</a></li>
             <li><a href="../vues/watch.html">Technology Watch</a></li>
             <li><a href="../vues/Documents.html">Documents</a></li>
-            <li><a href="../vues/contact.html">Contact</a></li>
+            <li>
+                <a href="https://github.com/Trotiflex" target="_blank">
+                    <img src="../public/image/github.png" alt="GitHub" class="github-icon" />
+                </a>
+            </li>
         </ul>
-        <div class="border-gradient"></div>
     </div>
 `;
 
@@ -42,14 +49,14 @@ const navbarContent = `
     });
 
 })();
+
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll(".navbar a");
-    const currentURL = window.location.href; // Récupère l'URL complète de la page
+    const currentURL = window.location.href;
 
     navLinks.forEach(link => {
-        // Compare l'URL complète du lien avec celle de la page actuelle
         if (link.href === currentURL) {
-            link.classList.add("active"); // Ajoute la classe "active"
+            link.classList.add("active");
         }
     });
 });
